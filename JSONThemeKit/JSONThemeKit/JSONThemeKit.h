0f40ee245327a8a6078bc2f6fe2b2b7565c28c37
Matt Revell <mattrevell82@me.com>
2017-01-12 18:06:09 +0000

    Add basic framework skeleton

---
+++ JSONThemeKit/JSONThemeKit/JSONThemeKit.h
@@ -0,0 +1,19 @@
+//
+//  JSONThemeKit.h
+//  JSONThemeKit
+//
+//  Created by Matt Revell on 12/01/2017.
+//  Copyright © 2017 Matt Revell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONThemeKit.
+FOUNDATION_EXPORT double JSONThemeKitVersionNumber;
+
+//! Project version string for JSONThemeKit.
+FOUNDATION_EXPORT const unsigned char JSONThemeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONThemeKit/PublicHeader.h>
+
+
