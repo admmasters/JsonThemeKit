258ddfd131b05192fb6004bb030e03f025f121f4
Matt Revell <mattrevell82@me.com>
2017-01-13 13:26:45 +0000

    Add a test for bar tint color

--- JSONThemeKit/JSONThemeKitTests/JSONThemeKitTests.swift
+++ JSONThemeKit/JSONThemeKitTests/JSONThemeKitTests.swift
@@ -20,59 +20,15 @@ class JSONThemeKitTests: XCTestCase {
     XCTAssert(actual.naiveIsEqual(.red), "\(actual) is not red")
     
   }
-  /*
-  func testThatWeCanSetTintColor() {
+  
+  func testThatBarTintColorCanBeSet() {
     
-    guard let jsonData = getNormalMockColorSettings()
-      else { return XCTFail("Can't read JSON") }
+    let jsonData: [String: String] = try! testBundle.getJSONData(filename: "MockColorSettings.json")
+    let theme = JSONTheme(json: jsonData)
+    let actual = theme!.barTintColor
     
-    guard let theme = JSONTheme(json: jsonData)
-      else { return XCTFail("Can't create Color Object") }
-    
-    let expected = "#FF0000".toColor()
-    let actual = theme.tintColor
-    
-    assert(actual.naiveIsEqual(expected), "\(actual) is not equal to \(expected))")
+    XCTAssert(actual.naiveIsEqual(.green), "\(actual) is not green")
     
   }
   
-  func testThatWeCanSetBarTintColor() {
-    
-    guard let jsonData = getNormalMockColorSettings()
-      else { return XCTFail("Can't read JSON") }
-    
-    guard let theme = JSONTheme(json: jsonData)
-      else { return XCTFail("Can't create Color Object") }
-    
-    let expected = "#00FF00".toColor()
-    let actual = theme.barTintColor
-    
-    assert(actual.naiveIsEqual(expected), "\(actual) is not equal to \(expected))")
-    
-  }
-  
-  func testThatWeCanSetStatusBarStyle() {
-    
-    guard let jsonData = getNormalMockColorSettings()
-      else { return XCTFail("Can't read JSON") }
-    
-    guard let theme = JSONTheme(json: jsonData)
-      else { return XCTFail("Can't create Color Object") }
-    
-    let expected: UIStatusBarStyle = .lightContent
-    let actual: UIStatusBarStyle = theme.statusBarStyle
-    
-    assert(actual == expected, "\(actual) is not equal to \(expected))")
-    
-  }
-  
-  private func getNormalMockColorSettings() -> [String: Any] {
-    let testBundle = Bundle(for: type(of: self))
-    do {
-      let result: [String: Any] = try testBundle.getJSONData(filename: "MockColorSettings.json")
-      return result
-    } catch {
-      return nil
-    }
-  }*/
 }
